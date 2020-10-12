1fd1afd09fe23f68553d9797bbcf18e2535b1f87
VoxPolly27 <39667433+VoxPolly27@users.noreply.github.com>
2020-10-12 10:55:44 +0200

    Completed walking skeleton

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -30,6 +30,11 @@ namespace Api
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -46,6 +51,8 @@ namespace Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
